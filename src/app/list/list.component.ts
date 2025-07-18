cc94219d04b9ceaa2bca9de59c2788c76a46ec0e
Yousef Gheibi <gheibiyousef2000@gmail.com>
2025-07-18 11:49:24 +0330

    completed list component

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Coffee } from '../../logic/Coffee';
+import { DataService } from '../data.service';
+import { JsonPipe } from '@angular/common';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [ JsonPipe, MatCardModule, MatButtonModule,MatIconModule],
   templateUrl: './list.component.html',
-  styleUrl: './list.component.scss'
+  styleUrl: './list.component.scss',
 })
-export class ListComponent {
+export class ListComponent implements OnInit {
+  list: Coffee[] = [];
+  constructor(private data: DataService) {}
 
+  ngOnInit(): void {
+    this.data.getList((list: Coffee[]) => {
+      this.list = list;
+    });
+  }
 }
